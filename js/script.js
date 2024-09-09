b8238971ae899d8e19bd3872ae33893e5e91a55f
HELAL15 <ah4459577@gmail.com>
2024-09-09 12:23:15 +0300

    final

--- js/script.js
+++ js/script.js
@@ -14,26 +14,10 @@ $(document).ready(function(){
 
 
 
-    // const navToggler = $("#nav-toggler");
-    // const collapse = $(".navbar-collapse");
-    // const close = $("#close");
-    
-    // navToggler.on("click", function(){
-    //   collapse.toggleClass("active");
-    // });
-    // close.on("click", function(){
-    //   collapse.toggleClass("active");
-    // });
-
-
-    
-    
-    // // loader
-    // $('#loading').fadeOut(500);
 
     new WOW().init();
 
-    // $('select').niceSelect();
+
 
 
 
@@ -78,32 +62,6 @@ $(document).ready(function(){
 
 
     
-
-// function showScrollTopBtn() {
-//   let scroll = window.scrollY;
-//   let isNavTop = scroll < 250;
-//   let scrollBtn = document.querySelector('.scrollTop');
-//   if ( isNavTop) {
-//     scrollBtn.classList.remove('show');
-    
-//   } else {
-//     scrollBtn.classList.add('show');
-//   }
-// }
-// let scrollBtn = document.querySelector('.scrollTop');
-
-// if(scrollBtn != null){
-//   window.addEventListener('scroll', showScrollTopBtn);
-//   scrollBtn.addEventListener("click", ()=>{
-//     window.scrollTo({top:0, left:0 });
-//   })
-// }
-
-
-
-
-
-
 
 
     //end
